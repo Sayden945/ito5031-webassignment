610e05193ced6d1395be1a6bb61888514028ed7b
Ayden Smith <122020846+Sayden945@users.noreply.github.com>
2025-11-25 18:34:57 +1100

    Initialize Firebase in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
 
 app.use(createPinia())
 app.use(router)
